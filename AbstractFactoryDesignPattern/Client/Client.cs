0f42476084eb25899310d73c9d3226d98b721efb
salikhafeez44 <47096464+salikhafeez44@users.noreply.github.com>
2023-11-08 02:00:59 -0600

    Abstract Factory Design Pattern

---
+++ AbstractFactoryDesignPattern/Client/Client.cs
@@ -0,0 +1,23 @@
+﻿using AbstractFactoryDesignPattern.Factory.ConcreteFactories;
+using AbstractFactoryDesignPattern.Factory.IAbstractFactory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactoryDesignPattern.Client
+{
+    internal class Client
+    {
+        public static void NewFactory(IFactory factory)
+        {
+            var keyboard = factory.Create1().Keyboard();
+            var monitor = factory.Create2().Monitor();
+
+            Console.WriteLine($"{keyboard}");
+            Console.WriteLine($"{monitor}");
+        }
+        
+    }
+}
